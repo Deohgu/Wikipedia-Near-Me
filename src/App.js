c53ca98044f4a7d9fd4633c968ba9bec7723f008
deohgu <dioggopsanttos@gmail.com>
2020-07-30 00:19:01 +0100

    Setting up, Fetching API data.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
-import styles from "./App.module.css"
+import GpsIcon from "./components/GpsIcon/GpsIcon";
+import Ripples from "./components/Ripples/Ripples";
+import { apiTesting } from "./api";
 
 const App = () => {
-  
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      return await apiTesting();
+    };
+    setArticles(fetchData());
+  }, []);
+
   return (
-  
+    <>
+      <GpsIcon />
+      <Ripples articles={articles} />
+    </>
   );
 };
+
+export default App;
