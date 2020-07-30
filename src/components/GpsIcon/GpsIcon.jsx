b42e0ca37682009de992be5c593eeb5330ea6fd0
deohgu <dioggopsanttos@gmail.com>
2020-07-30 22:16:07 +0100

    Learning how to use GeoLocation.

--- src/components/GpsIcon/GpsIcon.jsx
+++ src/components/GpsIcon/GpsIcon.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import styled from "styled-components";
 
 const GpsIcon = () => {
+  // When run asks for current position and callsback a function and gives it the value as a parameter
+  const testing = () => {
+    navigator.geolocation.getCurrentPosition(testingOutput);
+  };
+
+  // function called in testing which gets data in the parameter from navigator.geolocation.getCurrentPosition
+  //And the data is accessed as parameter.coords.latitude.
+  const testingOutput = (position) => {
+    console.log(position.coords.latitude + " " + position.coords.longitude);
+  };
+
+  // onClick runs a function
   return (
     <>
-      <button>GPS</button>
+      <button onClick={testing}>GPS</button>
     </>
   );
 };
