3d9e4cd22f1b7cb230fcaa7e1a3423fe8f5d9621
deohgu <dioggopsanttos@gmail.com>
2020-07-30 18:50:03 +0100

    Rendering API result to screen.

--- src/components/Ripples/Ripples.jsx
+++ src/components/Ripples/Ripples.jsx
@@ -4,10 +4,10 @@ import styled from "styled-components";
 const Ripples = (props) => {
   return (
     <>
-      // Why is this not working and getting an error?
-      {props.articles.map((curr) => (
-        <h1>{curr.title}</h1>
-      ))}
+      {console.log(props.articles)}
+      { props.articles.map((curr) => (
+          <h1>{curr.title}</h1>
+        ))}
     </>
   );
 };
