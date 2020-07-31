8d33e40143ce669934230e34d5890513c8ece686
deohgu <dioggopsanttos@gmail.com>
2020-07-31 23:28:43 +0100

    API using provided location.

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,24 @@
-const location = `41.118277%7C-8.027207`;
-
-const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gscoord=${location}&gsradius=10000&gslimit=20&gsnamespace=0&origin=*`;
 // gscoord=41.118277%7C-8.027207
 // | = %7C
 
-export const apiTesting = async () => {
+export const apiTesting = async (location) => {
+  const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gscoord=${location}&gsradius=10000&gslimit=20&gsnamespace=0&origin=*`;
+
   // const articles = [];
 
   // IIFE to contain the fetch and fill in the array outside of it.
   // Not sure if I need to pass to articles here, remains to be seen later.
   // It wasn't working because of this IIFE
   // (async () => {
+  if (location !== "") {
     const fetchData = await fetch(url);
     const {
       query: { geosearch },
     } = await fetchData.json();
     console.log(geosearch);
     return geosearch;
+  } else {
+    console.log("Location in api undefined.");
+  }
   // })();
 };
