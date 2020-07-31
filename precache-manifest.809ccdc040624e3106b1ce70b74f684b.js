016672d162c2eb15c3d77d1ee1db0ec2be8c91c2
deohgu <dioggopsanttos@gmail.com>
2020-07-31 22:29:10 +0100

    Updates

---
+++ precache-manifest.809ccdc040624e3106b1ce70b74f684b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0301e60ddf195cc70d5d7db7d672e8ce",
+    "url": "/wikipedia-near-me/index.html"
+  },
+  {
+    "revision": "4b745c2363f912f97d5d",
+    "url": "/wikipedia-near-me/static/js/2.55e503e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wikipedia-near-me/static/js/2.55e503e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575d51a9b20030672568",
+    "url": "/wikipedia-near-me/static/js/main.b7d73633.chunk.js"
+  },
+  {
+    "revision": "dc16e4334fb36af89b84",
+    "url": "/wikipedia-near-me/static/js/runtime-main.87a3d538.js"
+  }
+]);
