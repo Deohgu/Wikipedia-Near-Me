d1e2015e7f719ae615bd94d7fde7fee2136189a6
deohgu <dioggopsanttos@gmail.com>
2020-07-31 23:29:17 +0100

    Testing with Logs and next day note.

--- src/components/GpsIcon/GpsIcon.jsx
+++ src/components/GpsIcon/GpsIcon.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
-const GpsIcon = () => {
+// Next thing is to check out the Geolocation.watchPosition() for the heading option to see if the compass would work.
+// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation/watchPosition
+
+const GpsIcon = (props) => {
   // When run asks for current position and callsback a function and gives it the value as a parameter
   const testing = () => {
     navigator.geolocation.getCurrentPosition(testingOutput);
@@ -10,7 +13,13 @@ const GpsIcon = () => {
   // function called in testing which gets data in the parameter from navigator.geolocation.getCurrentPosition
   //And the data is accessed as parameter.coords.latitude.
   const testingOutput = (position) => {
-    console.log(position.coords.latitude + " " + position.coords.longitude);
+    console.log(
+      `Latitude = ${position.coords.latitude}, Longitude = ${position.coords.longitude}, Facing = ${position.coords.heading}`
+    );
+    props.setLocation(
+      position.coords.latitude + "%7C" + position.coords.longitude
+    );
+    props.testDisplay(position.coords.heading);
   };
 
   // onClick runs a function
