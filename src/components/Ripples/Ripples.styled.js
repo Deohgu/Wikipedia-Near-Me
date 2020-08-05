15615d4c4e19efe2ea85bbe0c0c9a79e29b55e08
deohgu <dioggopsanttos@gmail.com>
2020-08-05 13:39:39 +0100

    Adding math to calculate text position.

--- src/components/Ripples/Ripples.styled.js
+++ src/components/Ripples/Ripples.styled.js
@@ -1,8 +1,19 @@
 import styled from "styled-components";
 
+const mapWidthMet = 1000;
+
+const mapWidthDeg = (1 * mapWidthMet) / 111.32;
+
+// need to receive these items (lat and ownLat);
+const positionLatDeg = (lat * mapWidthDeg/2) / ownLat;
+
+const positionLatPx = (positionLatDeg * vw) / mapWidthDeg;
+
+// Then do the same for longitude or adapt it to work with both.
+
 export const Locations = styled.a`
   color: black;
   /* position: absolute;
-  right: ${(props) => (props.lat * 100) / 414 + "%"};
-  top: ${(props) => (props.lon * 100) / 736 + "%"}; */
+  right: ${(props) => positionLatPx + "px"};
+  top: ${(props) =>  positionPx + "px"}; */
 `;
