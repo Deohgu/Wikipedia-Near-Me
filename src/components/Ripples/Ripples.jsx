93eebb017f1b8bfbd58c26be4316f70c0a7161fe
deohgu <dioggopsanttos@gmail.com>
2020-08-06 22:43:41 +0100

    Visual help coords, fix props error.

--- src/components/Ripples/Ripples.jsx
+++ src/components/Ripples/Ripples.jsx
@@ -6,15 +6,17 @@ import { Locations } from "./Ripples.styled";
 const Ripples = (props) => {
   return (
     <>
+      <h1 style={{ margin: 0 }}>{`${props.lat} ${props.lon}`}</h1>
       {props.articles !== undefined
         ? props.articles.map((curr, index) => (
             <Locations
               key={index}
+              indx={index}
               href={
                 "https://en.wikipedia.org/wiki/" + curr.title.replace(" ", "_")
               }
               lat={curr.lat}
-              long={curr.lon}
+              lon={curr.lon}
             >
               {`${curr.title}     (${curr.lat}, ${curr.lon})     `}
             </Locations>
