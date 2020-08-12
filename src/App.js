86d2aa95561ffc97090dfc6e8a587033e90e1785
deohgu <dioggopsanttos@gmail.com>
2020-08-13 00:57:24 +0100

    Trying to get the coords at start of app.

--- src/App.js
+++ src/App.js
@@ -4,18 +4,33 @@ import { GlobalStyle, Wrapper } from "./App.styled";
 
 import GpsIcon from "./components/GpsIcon/GpsIcon";
 import Ripples from "./components/Ripples/Ripples";
-import { apiTesting } from "./api";
+import { locationAPI, apiTesting } from "./api";
 
 // To Do
-// Increase search radius?
-// Improve visual styling of GPS button and its name
-// Or Limit radius to viewable size and fetch when zoom level changes? Does pinching with fingers change zoom?
+// Trying for the useEffect to call the locationAPI instead of the button, currently it is a mess the code on the API index file. It's returning either undefined or with the testCoords an emtpy array. Might need an async function? Why is it undefined. When adding await it says that it is not necessary meaning that there isn't a promise meaning that the console.log() will not run before the things are executed.
 
 const App = () => {
   const [articles, setArticles] = useState([]);
   const [lat, setLat] = useState("");
   const [lon, setLon] = useState("");
+  const [coords, setCoords] = useState([85.77654, 172.212518]);
   const [zoom, setZoom] = useState(15);
+
+  const fetchData = async () => {
+    const fetchedData = await apiTesting(lat + "%7C" + lon);
+    setArticles(fetchedData);
+  };
+
+  useEffect(() => {
+    console.log(locationAPI());
+    // const fetchData = async () => {
+    //   console.log("Testing");
+    //   await setCoords(setTimeout(locationAPI(), 1000));
+    //   console.log(coords);
+    // };
+    // fetchData();
+  }, []);
+  // setTimeout(fetchData(), 1000);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,7 +45,13 @@ const App = () => {
       <GlobalStyle />
       <Wrapper>
         <GpsIcon setLat={setLat} setLon={setLon} />
-        <Ripples articles={articles} lat={lat} lon={lon} zoom={zoom} />
+        <Ripples
+          articles={articles}
+          lat={lat}
+          lon={lon}
+          coords={coords}
+          zoom={zoom}
+        />
       </Wrapper>
     </>
   );
--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,6 @@
-// gscoord=41.118277%7C-8.027207
-// | = %7C
-
 export const apiTesting = async (location) => {
-  const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gscoord=${location}&gsradius=10000&gslimit=20&gsnamespace=0&origin=*`;
+  const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gscoord=${location}&gsradius=10000&gslimit=10&gsnamespace=0&origin=*`;
 
-  // const articles = [];
-
-  // IIFE to contain the fetch and fill in the array outside of it.
-  // Not sure if I need to pass to articles here, remains to be seen later.
-  // It wasn't working because of this IIFE
-  // (async () => {
   if (location !== "%7C") {
     const fetchData = await fetch(url);
     const {
@@ -18,5 +9,27 @@ export const apiTesting = async (location) => {
     console.log(geosearch);
     return geosearch;
   }
-  // })();
+};
+
+export const locationAPI = () => {
+  let testCoords = ["empty"];
+
+  // When run asks for current position and callsback a function and gives it the value as a parameter
+  const testing = () => {
+    navigator.geolocation.getCurrentPosition(testingOutput);
+  };
+
+  // function called in testing which gets data in the parameter from navigator.geolocation.getCurrentPosition
+  //And the data is accessed as parameter.coords.latitude.
+  const testingOutput = (position) => {
+    console.log(
+      `Latitude = ${position.coords.latitude}, Longitude = ${position.coords.longitude}, Facing = ${position.coords.heading}`
+    );
+    console.log(position.coords.latitude);
+    testCoords = [position.coords.latitude, position.coords.longitude];
+    console.log(testCoords);
+  };
+  testing();
+  console.log(`Last Log - ${testCoords}`);
+  return testCoords;
 };
