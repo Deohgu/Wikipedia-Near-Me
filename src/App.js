987a2e69b704058db7f02f2394586f9a7c5a52af
deohgu <dioggopsanttos@gmail.com>
2020-08-12 18:17:14 +0100

    Improving Zoom level, To Do comments.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import GpsIcon from "./components/GpsIcon/GpsIcon";
 import Ripples from "./components/Ripples/Ripples";
 import { apiTesting } from "./api";
 
+// To Do
+// Increase search radius?
+// Improve visual styling of GPS button and its name
+// Or Limit radius to viewable size and fetch when zoom level changes? Does pinching with fingers change zoom?
+
 const App = () => {
   const [articles, setArticles] = useState([]);
   const [lat, setLat] = useState("");
   const [lon, setLon] = useState("");
-  const [zoom, setZoom] = useState(13);
+  const [zoom, setZoom] = useState(15);
 
   useEffect(() => {
     const fetchData = async () => {
