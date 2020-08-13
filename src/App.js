5f0999199c14b651cf582774cb43151688a49843
deohgu <dioggopsanttos@gmail.com>
2020-08-13 21:48:04 +0100

    Auto coords fetch, Heavy cleaning.

--- src/App.js
+++ src/App.js
@@ -1,58 +1,43 @@
 import React, { useEffect, useState } from "react";
 
-import { GlobalStyle, Wrapper } from "./App.styled";
+import { GlobalStyle } from "./App.styled";
 
-import GpsIcon from "./components/GpsIcon/GpsIcon";
-import Ripples from "./components/Ripples/Ripples";
-import { locationAPI, apiTesting } from "./api";
-
-// To Do
-// Trying for the useEffect to call the locationAPI instead of the button, currently it is a mess the code on the API index file. It's returning either undefined or with the testCoords an emtpy array. Might need an async function? Why is it undefined. When adding await it says that it is not necessary meaning that there isn't a promise meaning that the console.log() will not run before the things are executed.
+import { LeafletMap } from "./components/LeafletMap/LeafletMap";
+import { articlesFetch } from "./api";
 
 const App = () => {
   const [articles, setArticles] = useState([]);
-  const [lat, setLat] = useState("");
-  const [lon, setLon] = useState("");
-  const [coords, setCoords] = useState([85.77654, 172.212518]);
-  const [zoom, setZoom] = useState(15);
+  const [coords, setCoords] = useState([15.925994, -37.193399]); // Visually Neutral/Sea
+  const [zoom] = useState(15);
 
-  const fetchData = async () => {
-    const fetchedData = await apiTesting(lat + "%7C" + lon);
-    setArticles(fetchedData);
-  };
-
+  // When run asks for current position and callsback a function giving it the value as a parameter.
   useEffect(() => {
-    console.log(locationAPI());
-    // const fetchData = async () => {
-    //   console.log("Testing");
-    //   await setCoords(setTimeout(locationAPI(), 1000));
-    //   console.log(coords);
-    // };
-    // fetchData();
+    setTimeout(() => {
+      const asyncWrapper = async () => {
+        const coordsFetch = () => {
+          navigator.geolocation.getCurrentPosition(coordsReceived);
+        };
+        const coordsReceived = (position) => {
+          setCoords([position.coords.latitude, position.coords.longitude]);
+        };
+        coordsFetch();
+      };
+      asyncWrapper();
+    }, 1000);
   }, []);
-  // setTimeout(fetchData(), 1000);
 
   useEffect(() => {
     const fetchData = async () => {
-      const fetchedData = await apiTesting(lat + "%7C" + lon);
+      const fetchedData = await articlesFetch(coords[0] + "%7C" + coords[1]);
       setArticles(fetchedData);
     };
     fetchData();
-  }, [lat && lon]);
+  }, [coords]);
 
   return (
     <>
       <GlobalStyle />
-      <Wrapper>
-        <GpsIcon setLat={setLat} setLon={setLon} />
-        <Ripples
-          articles={articles}
-          lat={lat}
-          lon={lon}
-          coords={coords}
-          zoom={zoom}
-        />
-      </Wrapper>
+      <LeafletMap articles={articles} coords={coords} zoom={zoom} />
     </>
   );
 };
--- src/api/index.js
+++ src/api/index.js
@@ -1,35 +1,13 @@
-export const apiTesting = async (location) => {
+export const articlesFetch = async (location) => {
   const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=geosearch&gscoord=${location}&gsradius=10000&gslimit=10&gsnamespace=0&origin=*`;
 
-  if (location !== "%7C") {
+  // Not to fetch dummy location articles
+  if (location !== 15.925994 + "%7C" + -37.193399) {
     const fetchData = await fetch(url);
     const {
       query: { geosearch },
     } = await fetchData.json();
-    console.log(geosearch);
+    // console.log(geosearch);
     return geosearch;
   }
-};
-
-export const locationAPI = () => {
-  let testCoords = ["empty"];
-
-  // When run asks for current position and callsback a function and gives it the value as a parameter
-  const testing = () => {
-    navigator.geolocation.getCurrentPosition(testingOutput);
-  };
-
-  // function called in testing which gets data in the parameter from navigator.geolocation.getCurrentPosition
-  //And the data is accessed as parameter.coords.latitude.
-  const testingOutput = (position) => {
-    console.log(
-      `Latitude = ${position.coords.latitude}, Longitude = ${position.coords.longitude}, Facing = ${position.coords.heading}`
-    );
-    console.log(position.coords.latitude);
-    testCoords = [position.coords.latitude, position.coords.longitude];
-    console.log(testCoords);
-  };
-  testing();
-  console.log(`Last Log - ${testCoords}`);
-  return testCoords;
 };
