0815d5bd808cb785298aa1f5bac4f9ad838a99fb
deohgu <dioggopsanttos@gmail.com>
2020-08-13 21:49:09 +0100

    Replaced for auto coords fetch.

--- src/components/GpsIcon/GpsIcon.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-// The position must be calculate based on size not precise px size. How to define its current size in syntax?
-export const Icon = styled.button`
-  position: absolute;
-  top: calc(50% - (18px / 2));
-  right: calc(50% - (36.156px / 2));
-  z-index: 2;
-`;
