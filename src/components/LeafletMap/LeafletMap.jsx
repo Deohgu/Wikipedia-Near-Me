12b0d41dfbb85adea5f4e9747fe3cf29e1f8d85d
deohgu <dioggopsanttos@gmail.com>
2020-08-18 17:57:35 +0100

    Progress with the error. Read comments and Notion.

--- src/components/LeafletMap/LeafletMap.jsx
+++ src/components/LeafletMap/LeafletMap.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import L from "leaflet";
+import L, { latLngBounds } from "leaflet";
 import { TileLayer, Marker, Popup } from "react-leaflet";
 
 import { MapStyled } from "./LeafletMap.styled";
@@ -11,13 +11,36 @@ const myIcon = L.icon({
 });
 
 export const LeafletMap = (props) => {
+  const testTest = [
+    [38.858494, -9.081971],
+    [38.877043, -9.063705],
+    [38.87573, -9.044171],
+    [38.849149, -9.056733],
+  ];
+
+  const emptyArray = [
+    props.articles !== undefined
+      ? props.articles.map((curr) => [curr.lat, curr.lon])
+      : null,
+  ];
+  console.log(emptyArray);
+
   return (
-    <MapStyled center={props.coords} zoom={props.zoom}>
+    <MapStyled
+      // center={props.coords}
+      // zoom={props.zoom}
+      // It's working with this!
+      bounds={latLngBounds(emptyArray)}
+      // Replace with this to confirm that it is working.
+      // bounds={latLngBounds(testTest)}
+      // boundsOptions={{padding: [50, 50]}}
+    >
       {/* MapStyled = Map component of react-leaflet */}
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {console.log(props.articles)}
       {props.articles !== undefined
         ? props.articles.map((curr, index) => (
             <Marker
