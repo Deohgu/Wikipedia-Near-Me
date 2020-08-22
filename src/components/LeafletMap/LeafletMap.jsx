79fca92f80e2f26e2c7005ee2cef65325b8342e7
Deohgu <dioggopsanttos@gmail.com>
2020-08-23 00:29:37 +0100

    Fixed bounds and maxBounds now working together.

--- src/components/LeafletMap/LeafletMap.jsx
+++ src/components/LeafletMap/LeafletMap.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import L, {
-  latLngBounds,
-  bounds,
-  point,
-  fitBounds,
-  getBounds,
-  latLng,
-} from "leaflet";
+import L, { latLngBounds } from "leaflet";
 import { TileLayer, Marker, Popup } from "react-leaflet";
 
 import { MapStyled } from "./LeafletMap.styled";
@@ -18,52 +11,39 @@ const myIcon = L.icon({
 });
 
 export const LeafletMap = (props) => {
-  // Being used for testing purposes only.
-  const testTest = [
-    [38.858494, -9.081971],
-    [38.877043, -9.063705],
-    [38.87573, -9.044171],
-    [38.849149, -9.056733],
+  // Dummy coords to not output an error when receiving an empty bounds due to async API and sync components.
+  let emptyArray = [
+    [29.925994, -39.193599],
+    [29.945994, -39.143599],
   ];
-
-  // Dummy coords to not output an error when receiving an empty bounds.
-  //  Side note idea - Can javascript be added before bounds to define if bounds will be given or not?
-  let emptyArray = [[29.925994, -39.193599]];
 
   if (props.articles !== undefined) {
     props.articles.map((curr) => emptyArray.push([curr.lat, curr.lon]));
   }
 
   // Seems to work only in a separate if statment so be it
-  if (emptyArray.length === 11) {
+  if (emptyArray.length > 10) {
+    emptyArray = emptyArray.splice(1);
     emptyArray = emptyArray.splice(1);
   }
 
-  // Value seems correct now but it focuses only on one of the values.
-  console.log(emptyArray);
-
   return (
     <MapStyled
-      center={props.coords}
-      zoom={props.zoom}
       zoomSnap={0.1}
-      // bounds={latLngBounds(emptyArray)}
-
-      // boundsOptions={{ padding: [40, 40] }}
-
-      // Need to adapt zoom like bounds.
+      bounds={latLngBounds(emptyArray)}
+      boundsOptions={{ padding: [40, 40] }}
+      // Can I add some padding here too?
+      // ctrl-f for maxBounds on the docs.
       maxBounds={latLngBounds(emptyArray)}
-      // maxBoundsViscosity={1.0}
-      // Locking worked.
-      // dragging={false}
+      maxBoundsViscosity={1.0}
       zoomControl={false}
     >
-      {/* MapStyled = Map component of react-leaflet */}
+      {/* MapStyled = Map component of react-leaflet with styled-components */}
       <TileLayer
         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {console.log(props.articles)}
+      {/* {console.log(props.articles)} */}
       {props.articles !== undefined
         ? props.articles.map((curr, index) => (
             <Marker
